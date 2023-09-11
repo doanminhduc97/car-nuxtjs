b7760ad35c752701cf3e77fb4e477f65458fa7ac
doanminhduc97 <ducshooter.luvina@gmail.com>
2023-09-11 21:47:45 +0700

    v1

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -2,13 +2,13 @@
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Location</h3>
-            <h3 class="text-blue-400 capitalize">Toronto</h3>
-            <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-            <input type="text" class="border p-1 rounded">
-            <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
-                Apply
-            </button>
-            </div> -->
+            <h3 @click="updateModal('location')" class="text-blue-400 capitalize" id="location">{{ route.params.city }}</h3>
+            <div v-if="modal.location" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" id="popupLocation">
+                <input v-model="city" type="text" class="border p-1 rounded" id="inputLocation">
+                <button @click="onchangeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1" id="btnLocation">
+                    Apply
+                </button>
+            </div>
         </div>
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Make</h3>
@@ -20,3 +20,42 @@
         </div>
     </div>
 </template>
+
+<script setup>
+    const modal = ref({
+        make: false,
+        location: false,
+        price: false,
+    })
+    document.addEventListener('click', (e) => {
+        if (e.srcElement.id === 'location' || e.srcElement.id === 'inputLocation' || e.srcElement.id === 'btnLocation' || e.srcElement.id === 'popupLocation') {
+            modal.value.location = true;
+        } else {
+            modal.value.location = false;
+        }
+    })
+    
+    // onUnmounted(() => {
+    //     document.removeEventListener("click");
+    // })
+    const updateModal = (key) => {
+        modal.value[key] = !modal.value[key];
+    }
+
+    const city = ref('');
+    const route = useRoute();
+
+    const onchangeLocation = () => {
+        if(!city.value) return;
+        if (!isNaN(parseInt(city.value))) {
+            throw createError({
+                statusCode: 400,
+                message: 'Invalid ctiy format'
+            })
+        }
+        updateModal('location');
+        navigateTo(`/city/${city.value}/car/${route.params.make}`);
+        city.value = "";
+    }
+
+</script>
--- cartrader/pages/city/[city]/car.vue
+++ cartrader/pages/city/[city]/car.vue
@@ -5,8 +5,19 @@
         <!-- <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"> -->
             <div class="mt-32 flex">
                 <!-- Car side bar -->
-                <CarSideBar />
-                <NuxtPage />
+                <NuxtErrorBoundary>
+                    <CarSideBar />
+                    <NuxtPage />
+                    <template #error="{error}">
+                        <div class="text-center mx-auto flex flex-col">
+                            <h1 class="text-5xl text-red-600 mb-4">Sorry, something went wrong</h1>
+                            <code>{{ error }}</code>
+                            <button class="text-white bg-blue-400 px-10 py-3 rounded mt-4" @click="error.value = null">
+                                Go back
+                            </button>
+                        </div>
+                    </template>
+                </NuxtErrorBoundary>
             </div>
         <!-- </div> -->
     </div>
